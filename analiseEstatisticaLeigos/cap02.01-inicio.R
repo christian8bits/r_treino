ad8a743e114924ead1aead4247eb2144e1a05c84
ChristianBits <christian8bits@gmail.com>
2022-02-22 18:56:25 -0300

    comandos básicos R

---
+++ analiseEstatisticaLeigos/cap02.01-inicio.R
@@ -0,0 +1,19 @@
+print('diretorio de trabalho:')
+getwd()
+
+# atribui vetor 
+x <- c(3, 4, 5)
+a <- c(7, 3, 1)
+print(x)
+
+print('Soma de x:')
+sum(x)
+
+ls()
+
+print('Media de x:')
+mean(x)
+
+print('variancia de um vetor:')
+var(x)
+
